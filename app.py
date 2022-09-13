4f0d2fb35f00f0ea4f21469e4dab5cdb55227d72
Hong-HH <a1guskn7@gmail.com>
2022-09-13 18:40:13 +0900

    add_route_login

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask, request
 
 
+
+from config import Config
+
+
 app = Flask(__name__)
 
+# 환경변수 셋팅
+app.config.from_object(Config)
 
 
+
+# 페이지 경로 설정 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/login")
+def login():
+    
+    return "<p>Hello, This is Login Page!</p>"
 
 
 if __name__ == "__main__" :
